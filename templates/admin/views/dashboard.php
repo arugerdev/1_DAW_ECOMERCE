ecfbebbc0d643c37829bcdda95717a81d9cfee1c
arugerdev <alvaroch250@gmail.com>
2026-01-16 14:06:52 +0100

    Fix href

--- templates/admin/views/dashboard.php
+++ templates/admin/views/dashboard.php
@@ -45,7 +45,7 @@
                                 <div class="icon">
                                     <i class="ion ion-stats-bars"></i>
                                 </div>
-                                <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+                                <a href="/admin/products" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
                             </div>
                         </div>
 
